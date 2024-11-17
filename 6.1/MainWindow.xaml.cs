64988c4579c5a92ddd7e2e32de2438155c9047f3
Kemran <morpeh2005ka@gmail.com>
2024-11-17 18:22:37 +0300

    Я что то высрал

---
+++ 6.1/MainWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System.Text;
+using System.Windows;
+using MeasureLengthDeviceNamespace;
+
+namespace _5._1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private IMeasuringDevice device;
+        private Units unit = Units.Imperial;
+        public MainWindow() => InitializeComponent();
+        /// <summary>
+        /// Обработчик для createInstance_Click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void createInstance_Click(object sender, RoutedEventArgs e)
+        {
+            device = new MeasureMassDevice(unit);
+            logs.Content = "Device created";
+        }
+        /// <summary>
+        /// Когда пользователь прожимает один из радиобаттонов
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Radio_Button_Click(object sender, RoutedEventArgs e) => unit = (Imperial_Button.IsChecked == true) ? Units.Imperial : Units.Metric;
+        /// <summary>
+        /// обработчик для прожатия StartCollecting
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Start_Collecting_Click(object sender, RoutedEventArgs e)
+        {
+            logs.Content = "Please make device";
+            if (device != null) 
+            { 
+                device.StartCollecting();
+                logs.Content = "Start Collecting";
+            }
+        }
+        /// <summary>
+        /// обработчик для GetRawData_Click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void GetRawData_Click(object sender, RoutedEventArgs e)
+        {
+            logs.Content = "Please make device";
+            if (device != null)
+            {
+                StringBuilder res = new StringBuilder();
+                foreach (int i in device.GetRawData()) { res.Append($"{i} "); }
+                logs.Content = res.ToString();
+            }
+        }
+        /// <summary>
+        /// обработка для GetMetric
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void GetMetric_Click(object sender, RoutedEventArgs e)
+        {
+            logs.Content = "Please make device";
+            if (device != null)
+            {
+                logs.Content = $"MetricValue: {device.MetricValue()}";
+            }
+        }
+        /// <summary>
+        /// обработчик для GetMetric
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void GetImperial_Click(object sender, RoutedEventArgs e)
+        {
+            logs.Content = "Please make device";
+            if (device != null)
+            {
+                logs.Content = $"ImperialValue: {device.ImperialValue()}";
+            }
+        }
+        /// <summary>
+        /// Обработчик для Stop_Collecting_Click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Stop_Collecting_Click(object sender, RoutedEventArgs e)
+        {
+            logs.Content = "Please make device";
+            if (device != null)
+            {
+                device.StopCollecting();
+                logs.Content = $"Device stop collecting";
+            }
+        }
+    }
+}
---
+++ 6.1/MeasureDataDevice.cs
@@ -0,0 +1,101 @@
+﻿using DeviceControl;
+using System;
+using System.ComponentModel; // Для BackgroundWorker
+using System.IO; // Для StreamWriter
+using System.Timers;
+
+namespace MeasureLengthDeviceNamespace
+{
+    public abstract class MeasureDataDevice : IMeasuringDevice
+    {
+        protected Units unitsToUse;
+        protected int[] dataCaptured;
+        protected int mostRecentMeasure;
+        protected DeviceController? controller;
+        protected abstract DeviceType measurementType { get; }
+
+        private BackgroundWorker? dataCollector;
+        private StreamWriter? loggingFileWriter;
+        public event EventHandler? NewMeasurementTaken;
+
+        // Абстрактные методы для конкретных устройств
+        public abstract decimal MetricValue();
+        public abstract decimal ImperialValue();
+
+        public void StartCollecting()
+        {
+            controller = DeviceController.StartDevice(measurementType);
+            GetMeasurements();
+            loggingFileWriter = new StreamWriter(GetLoggingFile());
+            loggingFileWriter.WriteLine($"Time: {DateTime.Now}, Start Collecting");
+        }
+
+        public void StopCollecting()
+        {
+            if (controller != null)
+            {
+                DeviceController.StopDevice();
+                controller = null;
+                loggingFileWriter?.WriteLine($"Time: {DateTime.Now}, Stop Collecting");
+                loggingFileWriter.Close();
+            }
+        }
+
+        public int[] GetRawData()
+        {
+            return dataCaptured;
+        }
+
+        // Метод для получения измерений
+        private void GetMeasurements()
+        {
+            dataCollector = new BackgroundWorker();
+            dataCollector.DoWork += dataCollector_DoWork;
+            dataCollector.ProgressChanged += dataCollector_ProgressChanged;
+            dataCollector.RunWorkerAsync();
+        }
+
+        private void dataCollector_DoWork(object sender, DoWorkEventArgs e)
+        {
+            dataCaptured = new int[10];
+            int i = 0;
+            while (!dataCollector.CancellationPending)
+            {
+                // Получаем новое измерение от контроллера и сохраняем его в массив
+                dataCaptured[i % 10] = DeviceController.TakeMeasurement();
+                // Обновляем последнее измерение
+                mostRecentMeasure = dataCaptured[i % 10];
+                loggingFileWriter?.WriteLine($"Time: {DateTime.Now}, collect {mostRecentMeasure}");
+                i++;
+                System.Threading.Thread.Sleep(1000);
+            }
+
+        }
+        private void dataCollector_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            NewMeasurementTaken?.Invoke(this, EventArgs.Empty);
+        }
+        public void Dispose()
+        {
+            // Проверяем, что BackgroundWorker не null
+            if (dataCollector != null)
+            {
+                dataCollector.Dispose(); // Освобождаем ресурсы BackgroundWorker
+            }
+
+            // Закрываем файл логов, если он открыт
+            loggingFileWriter?.WriteLine($"Time: {DateTime.Now}, Dispose");
+            loggingFileWriter?.Close();
+        }
+        public string GetLoggingFile()
+        {
+            // Возвращаем путь к лог-файлу
+            return $"{LoggingFileName}.log";
+        }
+
+        public int[] DataCaptured => dataCaptured;
+        public Units UnitsToUse => unitsToUse;
+        public int MostRecentMeasure => mostRecentMeasure;
+        public string? LoggingFileName { get; set; }
+    }
+}
